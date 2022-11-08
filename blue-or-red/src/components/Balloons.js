79c7ef494af09beaf8dc62d51569d6a3276a49cc
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-11-08 14:45:44 -0800

    figured out string parcing from cookies to see what colour the user saw last

--- blue-or-red/src/components/Balloons.js
+++ blue-or-red/src/components/Balloons.js
@@ -1,14 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
 const Balloons = () => {
 
+  const randoColour = () => {
+    return Math.floor(Math.random(2)) === 1 ? 'blue' : 'red';
+  };
+
+  const cookieCheck = () => {
+    if (document.cookie) {
+      let cookieColour = document.cookie
+        .toString()
+        .split(';')
+        .filter((cookie) => cookie.includes('colour'));
+      return cookieColour.includes('blue') ? 'blue' : 'red';
+    } else {
+      randoColour();
+    }
+  };
+
+  useEffect(()=> {
+    cookieCheck();
+  })
+
   return (
-    <div>
-      Balloons
-    </div>
+<div>Balloons</div>
   );
 };
 
 export default Balloons;
-
-
--- blue-or-red/src/script.js
+++ blue-or-red/src/script.js
@@ -1,7 +1,26 @@
-document.cookie = 'name=munster; expires=' + new Date(2023, 0, 1).toUTCString();
 
-document.cookie = 'petName=munsty; expires=' + new Date(2023, 0, 1).toUTCString();
+document.cookie = 'colour=red; expires=' + new Date(2023, 0, 1).toUTCString();
 
-console.log(document.cookie)
+//console.log(document.cookie.toString().split(';').filter((cookie) => cookie.includes('colour')))
 
+console.log("COOKIE STRING", document.cookie
+  .toString()
+  .split(';')
+  .filter((cookie) => cookie.includes('colour')))
+
+
+  const cookieCheck = () => {
+    if (document.cookie) {
+      let cookieColour = document.cookie
+        .toString()
+        .split(';')
+        .filter((cookie) => cookie.includes('colour'));
+        console.log("COOKIE COLOUR", cookieColour)
+      console.log(cookieColour.toString().includes('blue') ? 'blue' : 'red');
+    } else {
+      console.log('randoColor')
+    }
+  };
+
+  cookieCheck()
 

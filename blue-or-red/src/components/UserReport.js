5990713fddf5d1ff9f7d1dfe1ff9213522b5820c
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-11-09 22:41:42 -0800

    its working but adding 2 not 1

--- blue-or-red/src/components/UserReport.js
+++ blue-or-red/src/components/UserReport.js
@@ -3,24 +3,25 @@ import Modal from './Modal';
 
 const UserReport = () => {
   const [showModal, setModalIsShowing] = useState(false);
+  const [timesVisited, setTimesVisited] = useState(0);
   // const [plural, setPlural] = useState(false);
   //let plural = false;
 
   //this function will find the cookie created to track the number of times a user has visited this page in their browser and parse the cookie string for the number of times the user saw either colour and set it to state
-  const parceTimesVisited = () => {
-    let cookieVisits = document.cookie
-      .toString()
-      .split(';')
-      .filter((cookie) => cookie.includes('timesVisited'));
-    let str = cookieVisits[0];
-    str = str.split('');
-    let cut = str.indexOf('=');
-    let num = str.slice(cut + 1);
-    let joinedNum = num.join('');
-    return parseInt(joinedNum);
-  };
+  // const parceTimesVisited = () => {
+  //   let cookieVisits = document.cookie
+  //     .toString()
+  //     .split(';')
+  //     .filter((cookie) => cookie.includes('timesVisited'));
+  //   let str = cookieVisits[0];
+  //   str = str.split('');
+  //   let cut = str.indexOf('=');
+  //   let num = str.slice(cut + 1);
+  //   let joinedNum = num.join('');
+  //   return parseInt(joinedNum);
+  // };
 
-  let timesVisited = parceTimesVisited();
+  //let timesVisited = parceTimesVisited();
 
   //function to increase the value of the cookie tracking the number of times a user visited the page
   const upCookieCount = () => {
@@ -33,9 +34,11 @@ const UserReport = () => {
     let cut = str.indexOf('=');
     let num = str.slice(cut + 1);
     let joinedNum = num.join('');
+    let int = parseInt(joinedNum) + 1
     document.cookie =
-      `timesVisited=${parseInt(joinedNum)+1}; expires=` +
+      `timesVisited=${int}; expires=` +
       new Date(2023, 0, 1).toUTCString();
+      setTimesVisited(int)
   };
 
   //function to run when generate report is clicked that will pop up modal and propogate with the number of times the user has visited the page
@@ -60,8 +63,6 @@ const UserReport = () => {
   useEffect(() => {
     pageLoad();
   }, []);
-
-  //function to clear cookies from Modal?
 
   return (
     <>
